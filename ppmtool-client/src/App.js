4a411590dbcf8824e946efb330ea134f87d07b69
JohnIrle <irlejohn@gmail.com>
2020-04-01 13:29:03 -0500

    Navbar and Project Item components.

--- ppmtool-client/src/App.js
+++ ppmtool-client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Dashboard from "./components/Dashboard";
+import NavBar from "./components/Layout/Header";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Dashboard />
     </div>
   );
---
+++ ppmtool-client/src/components/Project/ProjectItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class ProjectItem extends Component {
+  render() {
+    return <div><h2>ProjectItem</h2></div>
+  }
+}
+
+export default ProjectItem

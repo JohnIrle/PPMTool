c0dfb4066f17f26df42066f0b436beea9007c3c8
JohnIrle <irlejohn@gmail.com>
2020-04-01 13:52:36 -0500

    Add bootstrap and style dashboard, projectitem and navbar.

--- ppmtool-client/src/App.js
+++ ppmtool-client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Dashboard from "./components/Dashboard";
 import NavBar from "./components/Layout/Header";
 

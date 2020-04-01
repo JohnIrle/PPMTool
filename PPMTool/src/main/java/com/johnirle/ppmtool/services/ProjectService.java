ddc32b96b5516f1cdd973fd4c75de038a2eaba31
JohnIrle <irlejohn@gmail.com>
2020-04-01 11:53:26 -0500

    Add exception and return message for Identifier not existing.

--- PPMTool/src/main/java/com/johnirle/ppmtool/services/ProjectService.java
+++ PPMTool/src/main/java/com/johnirle/ppmtool/services/ProjectService.java
@@ -24,4 +24,15 @@ public class ProjectService {
       throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
     }
   }
+
+  public Project findProjectByIdentifier(String projectId) {
+
+    Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+    if (project == null) {
+      throw new ProjectIdException("Project Id '"+projectId.toUpperCase()+"' does not exist");
+    }
+
+    return project;
+  }
 }

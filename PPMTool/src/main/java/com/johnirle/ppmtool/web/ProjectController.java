1816c605bef1e515e76220389d35019e4be730aa
JohnIrle <irlejohn@gmail.com>
2020-04-01 18:23:51 -0500

    add cross origin

--- PPMTool/src/main/java/com/johnirle/ppmtool/web/ProjectController.java
+++ PPMTool/src/main/java/com/johnirle/ppmtool/web/ProjectController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -19,6 +18,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
+@CrossOrigin
 public class ProjectController {
 
   @Autowired

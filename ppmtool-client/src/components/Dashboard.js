b7d3ca61c386620f02118b8681578e3986d42749
JohnIrle <irlejohn@gmail.com>
2020-04-02 09:42:06 -0500

    Map projects from redux state to project Item components.

--- ppmtool-client/src/components/Dashboard.js
+++ ppmtool-client/src/components/Dashboard.js
@@ -13,6 +13,8 @@ class Dashboard extends Component {
   }
 
   render() {
+    const { projects } = this.props.project
+
     return (
       <div className="projects">
         <div className="container">
@@ -22,9 +24,9 @@ class Dashboard extends Component {
               <CreateProjectButton />
               <br />
               <hr />
-
-              <ProjectItem />
-
+              {projects.map(project => {
+                return <ProjectItem key={project.id} project={project} />
+              })}
             </div>
           </div>
         </div>

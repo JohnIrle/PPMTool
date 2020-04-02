04dd1af9b5f2a9579d15aa0c9ba402061115964b
JohnIrle <irlejohn@gmail.com>
2020-04-02 10:33:39 -0500

    Get project action, connect UpdateProject to redux state.

--- ppmtool-client/src/actions/projectActions.js
+++ ppmtool-client/src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { GET_ERRORS, GET_PROJECTS } from "./types"
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types"
 
 export const createProject = (project, history) => async dispatch => {
   await axios.post("http://localhost:8080/api/project", project)
@@ -23,5 +23,14 @@ export const getProjects = () => async dispatch => {
         payload: res.data
       })
     )
+}
+
+export const getProject = (id, history) => async dispatch => {
+  await axios.get(`http://localhost:8080/api/project/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROJECT,
+        payload: res.data
+      }))
 }
 

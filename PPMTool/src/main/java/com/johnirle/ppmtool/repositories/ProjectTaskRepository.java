85f17794de83f6afc76ca0376bfcea4a000abf83
JohnIrle <irlejohn@gmail.com>
2020-04-02 20:55:24 -0500

    Change to MySQL and finish git all tasks.

--- PPMTool/src/main/java/com/johnirle/ppmtool/repositories/ProjectTaskRepository.java
+++ PPMTool/src/main/java/com/johnirle/ppmtool/repositories/ProjectTaskRepository.java
@@ -7,6 +7,10 @@ import com.johnirle.ppmtool.domain.ProjectTask;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+
+  List<ProjectTask> findByProjectIdentifierOrderByPriority(String id);
 }

db7e96eea49e7ef216b947ca2ef1b720bac4e4c6
JohnIrle <irlejohn@gmail.com>
2020-04-07 23:24:26 -0500

    initial security config.

---
+++ PPMTool/src/main/java/com/johnirle/ppmtool/exceptions/InvalidLoginResponse.java
@@ -0,0 +1,31 @@
+package com.johnirle.ppmtool.exceptions;
+// 
+// John Irle
+// 07 April 2020
+
+public class InvalidLoginResponse {
+  private String username;
+  private String password;
+
+  public InvalidLoginResponse() {
+    this.username = "Invalid Username";
+    this.password = "Invlaid Password";
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
+

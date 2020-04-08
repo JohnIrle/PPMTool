c6406c6dc6ebcea04d28c23e0ae64af95b182043
JohnIrle <irlejohn@gmail.com>
2020-04-08 17:33:02 -0500

    More Security Config

--- PPMTool/src/main/java/com/johnirle/ppmtool/repositories/UserRepository.java
+++ PPMTool/src/main/java/com/johnirle/ppmtool/repositories/UserRepository.java
@@ -6,9 +6,10 @@ package com.johnirle.ppmtool.repositories;
 import com.johnirle.ppmtool.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
+  User findByUsername(String username);
+  User getById(Long id);
 }

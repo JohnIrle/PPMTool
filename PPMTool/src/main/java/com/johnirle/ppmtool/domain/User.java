a7099db1e4ebfb85d8d0cf98c39b4c9bd9f877b6
JohnIrle <irlejohn@gmail.com>
2020-04-07 23:04:15 -0500

    Initial files and depencencies for security.

---
+++ PPMTool/src/main/java/com/johnirle/ppmtool/domain/User.java
@@ -0,0 +1,7 @@
+package com.johnirle.ppmtool.domain;
+// 
+// John Irle
+// 07 April 2020
+
+public class User {
+}
---
+++ PPMTool/src/main/java/com/johnirle/ppmtool/web/UserController.java
@@ -0,0 +1,7 @@
+package com.johnirle.ppmtool.web;
+// 
+// John Irle
+// 07 April 2020
+
+public class UserController {
+}
